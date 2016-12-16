012f863085acc45c2062a98d556d9ab9ba83492a
visdark <visdark@hotmail.com>
2016-12-16 19:11:42 +0800

    16-12-15 初始化构建

--- README.md
+++ README.md
@@ -35,4 +35,9 @@ UI 框架和字体库
 * BrowserSync：npm install --save-dev browser-sync 　浏览器同步测试
 * gulp-imagemin: 压缩图片大小
 * sass和less插件：npm install --save-dev gulp-sass css构建工具
-* css压缩：npm install gulp-minify-css
+* css压缩：npm install gulp-minify-css 现在这个改版名称了 变成npm install gulp-clean-css --save-dev 
+* 头文件说明：npm install --save-dev gulp-header 可以在所有文件中加入版权标记
+* js压缩：npm install --save-dev gulp-uglify 压缩JS
+* 重命名文件：npm install --save-dev gulp-rename 
+* 合并文件：npm install --save-dev gulp-concat
+* 清除文件：npm install --save-dev gulp-clean 删除更新中的文件
--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,43 @@
 //导入工具包
 var gulp = require('gulp'), //构建gulp
     less = require('gulp-less'); //less支持
+    pkg = require('./package.json'); //配置数据
+    clean = require('gulp-clean'); //清除多余文件
+    uglify = require('gulp-uglify');  // js压缩
+    cleanCSS = require('gulp-clean-css'); //压缩CSS
+    header = require('gulp-header'); //版权插入声明
+    rename = require('gulp-rename'); //重命名支持
+    concat = require('gulp-concat'); //合并文件
+    runSequence = require('run-sequence'); //异步执行
     browserSync = require('browser-sync').create(); //浏览器测试
-// cssmin = require('gulp-minify-css'); //css压缩
-//声明任务
-//任务1：将less转换并压缩css
-gulp.task('testLess', function () {
-    gulp.src('src/less/normalize.less')
+
+
+//开始任务
+
+//任务：Less文件转化css
+gulp.task('vis-less', function(){
+    gulp.src('./src/less/vis-normalize.less')
         .pipe(less())
-       // .pipe(cssmin())
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('./dist/css/'))
+
+    gulp.src('./src/less/vis-visi.less')
+        .pipe(less())
+        .pipe(gulp.dest('./dist/css/'))
+
 });
-//任务2：浏览器测试
-gulp.task('browser-sync', function() {
+//任务：清除多余css
+gulp.task('vis-css', function(){
+    gulp.src(['./dist/css/vis-normalize.css','./dist/css/vis-visi.css'])
+        .pipe(concat('visi.css'))
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('./dist/css/'));
+});
+
+
+//任务：浏览器测试
+gulp.task('vis-browser', function() {
     browserSync.init({
         server: {
             baseDir: "./dist/"
@@ -24,6 +49,9 @@ gulp.task('browser-sync', function() {
     });
 });
 
-//合并执行任务
-gulp.task('default', ['testLess','browser-sync']);
 
+//顺序执行任务
+
+gulp.task('default', function(cb) {
+    runSequence('vis-less', 'vis-css', 'vis-browser', cb);
+});
