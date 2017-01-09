d0d814b23d7664744f20c9282f70b24668c6e570
visdark <visdark@hotmail.com>
2017-01-09 13:14:25 +0800

    17-01-09 更新产品页

--- src/js/vis4-luyou.js
+++ src/js/vis4-luyou.js
@@ -1,76 +1,46 @@
-// 0. 如果使用模块化机制编程，導入Vue和VueRouter，要调用 Vue.use(VueRouter)
-
-// 1. 定义（路由）组件。
-// 可以从其他文件 import 进来
-const pro = { template: `
+//产品列表内容表
+const pro1 = { template: `
 <transition name="fade">
-<div class="pro">
-产品介绍 {{ $route.params.id }}
-
+<div>
+外汇的相关说明
 </div>
 </transition>
 `};
-const Bar = { template: '<div>bar</div>' };
-const User = {
-    template: '<div>User  后面的是按照路径ID赋值的{{ $route.params.id }}<router-view></router-view></div>'
-};
-
-
-// 2. 定义路由
-// 每个路由应该映射一个组件。 其中"component" 可以是
-// 通过 Vue.extend() 创建的组件构造器，
-// 或者，只是一个组件配置对象。
-// 我们晚点再讨论嵌套路由。
+const pro2 = { template: '<div>贵金属</div>' };
+const pro3 = { template: '<div>能源</div>' };
+const pro4 = { template: '<div>股指</div>' };
+const pro5 = { template: '<div>农产品</div>' };
+const pro6 = { template: '<div>差价合约</div>' };
+const pro7 = { template: '<div>原油</div>' };
+const pro8 = { template: '<div>其他</div>' };
+//产品列表路由表
 const routes = [
-    { path: '/:id', component: pro },
-    { path: '/go', component: Bar }
+    { path: '/1', component: pro1 },
+    { path: '/2', component: pro2 },
+    { path: '/3', component: pro3 },
+    { path: '/4', component: pro4 },
+    { path: '/5', component: pro5 },
+    { path: '/6', component: pro6 },
+    { path: '/7', component: pro7 },
+    { path: '/8', component: pro8 }
+
 ];
 
-// 3. 创建 router 实例，然后传 `routes` 配置
-// 你还可以传别的配置参数, 不过先这么简单着吧。
+// 创建 router
 const router = new VueRouter({
     routes // （缩写）相当于 routes: routes
 });
-
-// 4. 创建和挂载根实例。
-// 记得要通过 router 配置参数注入路由，
-// 从而让整个应用都有路由功能
-const app = new Vue({
-    router
-}).$mount('#prohtml');
-
-// 现在，应用已经启动了！
+// 开启路由模板模式，挂载ing
 new Vue({
     router,
     template: `
     <div id="app101">
-      <h1>Transitions</h1>
- <ul>
-        <li><router-link to="/">/</router-link></li>
-        <li><router-link to="/1">/1</router-link></li>
-        <li><router-link to="/2">/2</router-link></li>
-        <li><router-link to="/go">/go</router-link></li>
-      </ul>
+    <div class="tit"><i class="visiis vis-notice"></i> 产品中心</div>
+      <div class="con"></div>
       <transition name="fade" mode="out-in">
         <router-view class="view"></router-view>
       </transition>
     </div>
-
-  <div class="post">
-    <div class="loading" v-if="loading">
-      Loading...
-    </div>
-
-    <div v-if="error" class="error">
-      {{ error }}
-    </div>
-
-    <div v-if="post" class="content">
-      <h2>{{ post.title }}</h2>
-      <p>{{ post.body }}</p>
-    </div>
-  </div>
-
   `
 }).$mount('#app101');
 
